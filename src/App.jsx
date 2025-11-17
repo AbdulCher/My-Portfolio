b640c19b554bf2451af07bf143513c8b3a9cc8d3
N Diaye C Abdul <abdul13un@gmail.com>
2025-11-17 22:08:23 +0100

    Ajout des sections et du Haeder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import React from "react";
+import Header from "./components/Header";
 
-import './App.css'
 
-export default function App() {
+function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-500 text-white">
-      Tailwind fonctionne !
+    <div className="scroll-smooth snap-y snap-mandatory h-screen overflow-scroll">
+      <Header />
+
+      <section id="accueil" className="snap-start h-screen bg-blue-500 flex items-center justify-center">
+        <h1 className="text-5xl text-white">Accueil</h1>
+      </section>
+
+      <section
+        id="portfolio"
+        className="snap-start h-screen bg-green-500 flex items-center justify-center"
+      >
+        <h1 className="text-5xl text-white">Portfolio</h1>
+      </section>
+
+      <section
+        id="apropos"
+        className="snap-start h-screen bg-purple-500 flex items-center justify-center"
+      >
+        <h1 className="text-5xl text-white">À propos</h1>
+      </section>
+
+      <section
+        id="contact"
+        className="snap-start h-screen bg-orange-500 flex items-center justify-center"
+      >
+        <h1 className="text-5xl text-white">Contact</h1>
+      </section>
     </div>
   );
 }
 
-
+export default App;
